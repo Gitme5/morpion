4762f8110f120277bcc5177342508200fe98fa50
Yoni <y_benatar@hotmail.com>
2019-10-17 20:28:44 +0200

    Update with clear

--- Readme.md
+++ Readme.md
@@ -1,1 +1,10 @@
-This is a Ruby Program
+MORPION POO
+
+This is a morpion game.
+Each player enters its name. The game decides who starts and what symbol each user will use.
+The first to align 3 symbols wins.
+If the table is filled without winner we have a draw.
+
+TO DO :
+- Handle wrong entries from user => None existing case
+- Handle wrong entries from user => Not empty case
--- lib/app/application.rb
+++ lib/app/application.rb
@@ -8,25 +8,29 @@ class Application
 
 	def perform
 
+		system 'clear'
 		my_game = Game.new #On lance une partie - on récupére les noms des joueurs. Joueur 1 joue avec les X et Joue avec les O.
 		current_player = my_game.who_starts? #On détermine qui commence la partie
 
-		show = Show.new
-		show.display_board([" "," "," "," "," "," "," "," "," "])
+		show = Show.new #Initialise display of the game table
+		system 'clear'
+		show.display_board([" "," "," "," "," "," "," "," "," "]) #Initialise first array
 
-		my_board = Board.new
-		index = my_board.ask_for_move
+		my_board = Board.new # Initialisation de la classe board
+		index = my_board.ask_for_move #Creation de l'index pour determiner dans quelle case le joueur veut jouer.
 		
 
-		array = my_board.move(index,current_player.symbol)
-		show.display_board(array)
+		array = my_board.move(index,current_player.symbol) #On crée le tableau des entrée joueur
+		system 'clear'
+		show.display_board(array) # Affichage du tableau
 		
-		while (my_board.victory(array,my_board.count_turn) == "")
-			who_plays = my_game.turn
-			index = my_board.ask_for_move
-			array = my_board.move(index,who_plays.symbol)
-			show.display_board(array)
-			victory = my_board.victory(array,my_board.count_turn)
+		while (my_board.victory(array,my_board.count_turn) == "") # Tant que personne ne gagne ou que le tableau n'est pas plein
+			who_plays = my_game.turn # Definir qui va jouer au prochain tour
+			index = my_board.ask_for_move #Creation de l'index pour determiner dans quelle case le joueur veut jouer.
+			array = my_board.move(index,who_plays.symbol) #On crée le tableau des entrée joueur
+			system 'clear'
+			show.display_board(array) # Affichage du tableau
+			victory = my_board.victory(array,my_board.count_turn) # On verifie si l'on a un vainqueur
 		end
 
 		if victory == "MATCH NUL"
